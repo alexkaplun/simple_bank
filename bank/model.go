566c0398716a989d9665f2b33186e0ea28cc7207
vsko <virakochi@gmail.com>
2018-12-17 10:06:03 +0200

    Business logic and tests

---
+++ bank/model.go
@@ -0,0 +1,106 @@
+package bank
+
+import (
+	"errors"
+	"github.com/google/uuid"
+	"math/big"
+	"sync"
+	"time"
+)
+
+type Account struct {
+	createdAt time.Time
+	updatedAt time.Time
+	balance   *big.Int
+}
+
+type Bank struct {
+	accounts map[uuid.UUID]*Account
+	mu       *sync.Mutex
+}
+
+var bank = &Bank{
+	accounts: make(map[uuid.UUID]*Account),
+	mu:       &sync.Mutex{}}
+
+func GetBank() *Bank {
+	return bank
+}
+
+func (b *Bank) CreateAccount(balance big.Int) (uuid.UUID, error) {
+	if balance.Cmp(new(big.Int)) == -1 {
+		return uuid.Nil, errors.New("Can't be negative balance")
+	}
+
+	newId := uuid.New()
+	if b.IdExists(newId) {
+		return uuid.Nil, errors.New("Can't generate Account ID")
+	}
+
+	b.mu.Lock()
+	newBalance := balance
+
+	b.accounts[newId] = &Account{
+		createdAt: time.Now(),
+		updatedAt: time.Now(),
+		balance:   &newBalance,
+	}
+	b.mu.Unlock()
+
+	return newId, nil
+}
+
+func (b *Bank) GetAccountBalance(id uuid.UUID) (string, error) {
+	// checking if account exists
+	ac, ok := b.accounts[id]
+	if !ok {
+		return "", errors.New("No account found")
+	}
+
+	b.mu.Lock()
+	balance := ac.balance
+	b.mu.Unlock()
+
+	return balance.String(), nil
+}
+
+func (b *Bank) IdExists(id uuid.UUID) bool {
+	_, ok := b.accounts[id]
+	return ok
+}
+
+func (b *Bank) Transfer(from uuid.UUID, to uuid.UUID, amount big.Int) error {
+	// checking 0 or negative amount
+	if amount.Cmp(new(big.Int)) <= 0 {
+		return errors.New("Can't be negative balance")
+	}
+
+	if !b.IdExists(to) {
+		return errors.New("Terminating account not found")
+	}
+
+	if !b.IdExists(from) {
+		return errors.New("Originating account not found")
+	} else {
+
+		b.mu.Lock()
+		defer b.mu.Unlock()
+
+		// Check of from has enough balance
+		fromBalance := b.accounts[from].balance
+		if amount.Cmp(fromBalance) == 1 {
+			return errors.New("Originating balance not enough")
+		}
+
+		//all validated, let's transfer
+		fromBalance.Sub(fromBalance, &amount)
+		b.accounts[from].updatedAt = time.Now()
+
+		toBalance := b.accounts[to].balance
+		toBalance.Add(toBalance, &amount)
+		b.accounts[to].updatedAt = time.Now()
+
+		return nil
+	}
+
+}
---
+++ bank/model_test.go
@@ -0,0 +1,206 @@
+package bank
+
+import (
+	"github.com/google/uuid"
+	"math/big"
+	"sync"
+	"testing"
+	"time"
+)
+
+var ids = []string{
+	"714252f0-718b-44d9-a349-7bb30276bd95",
+	"fc87c82f-46a1-4a38-b487-289bad552b65",
+	"ea90026f-3272-4f2b-bef3-c84293569828",
+	"c6a48141-1fcd-404c-9a9a-9a5a95e0d2af",
+	"b636ab15-1ded-4e55-9fa4-e773c11dc0d6",
+	"bf46513e-a4bb-4fe8-b80d-e26c71fc55e4",
+	"05fed8ad-3581-48f3-89a8-fcb29b672fe0",
+	"cd49335f-71bb-4856-b3be-6babf437064e",
+	"b1e6ded9-d6b1-4966-a537-fbb10a891b67",
+	////////////////////////////////////////
+	"bc7b1e1b-e4d9-4176-b606-cf58847a0d22",
+}
+
+var balances = []string{
+	"0",
+	"1",
+	"1000",
+	"50000",
+	"5000000",
+	"5000000",
+	"1000000000000000000000000000000000",
+	"12121212",
+	"34234234234",
+	///////////////////////////////////////
+	"50",
+}
+
+var testBank = &Bank{
+	accounts: make(map[uuid.UUID]*Account),
+	mu:       &sync.Mutex{},
+}
+
+func init() {
+	for i := 0; i < 9; i++ {
+		b := new(big.Int)
+		b.SetString(balances[i], 10)
+		acc := Account{
+			createdAt: time.Now(),
+			updatedAt: time.Now(),
+			balance:   b,
+		}
+		str, _ := uuid.Parse(ids[i])
+		testBank.accounts[str] = &acc
+	}
+}
+
+func TestBank_CreateAccount(t *testing.T) {
+	//fmt.Println(len(testBank.accounts))
+
+	b := new(big.Int)
+	b.SetString("1000", 10)
+	uid, err := testBank.CreateAccount(*b)
+
+	if err != nil {
+		t.Errorf("Can't create account with uid %s", uid.String())
+	} else {
+
+		createdBalance := testBank.accounts[uid].balance
+		if createdBalance.Cmp(b) != 0 {
+			t.Errorf("New account balance wrong. Got %s, expected %s", createdBalance.String(), b.String())
+		}
+	}
+}
+
+func TestBank_CreateAccountBalances(t *testing.T) {
+	b := new(big.Int)
+
+	// test zero balance
+	b.SetString("0", 10)
+	uid, err := testBank.CreateAccount(*b)
+
+	if err != nil {
+		t.Errorf("Can't create account with uid %s", uid.String())
+	} else {
+
+		createdBalance := testBank.accounts[uid].balance
+		if createdBalance.Cmp(b) != 0 {
+			t.Errorf("New account balance wrong. Got %s, expected %s", createdBalance.String(), b.String())
+		}
+
+	}
+
+	// test very big balance
+	b.SetString("9999999999999999999999999999999999999", 10)
+	uid, err = testBank.CreateAccount(*b)
+
+	if err != nil {
+		t.Errorf("Can't create account with uid %s", uid.String())
+	} else {
+
+		createdBalance := testBank.accounts[uid].balance
+		if createdBalance.Cmp(b) != 0 {
+			t.Errorf("New account balance wrong. Got %s, expected %s", createdBalance.String(), b.String())
+		}
+	}
+
+	// test  negative balance
+	b.SetString("-10", 10)
+	uid, err = testBank.CreateAccount(*b)
+
+	if err == nil {
+		t.Errorf("Should not allow create negative account")
+	}
+}
+
+func TestBank_GetAccountBalance(t *testing.T) {
+	uid, _ := uuid.Parse("05fed8ad-3581-48f3-89a8-fcb29b672fe0")
+	gotBalance, _ := testBank.GetAccountBalance(uid)
+
+	if gotBalance != "1000000000000000000000000000000000" {
+		t.Errorf("Get Balance error. Got %s expected 1000000000000000000000000000000000", gotBalance)
+	}
+}
+
+func TestBank_GetAccountBalanceNoAccount(t *testing.T) {
+	uid, _ := uuid.Parse("11111111-1111-1111-1111-1111111111")
+	_, err := testBank.GetAccountBalance(uid)
+
+	if err == nil {
+	 	t.Errorf("Get balance error, got balance on non existing account")
+	}
+}
+
+func TestBank_TransferFromNotExists(t *testing.T) {
+	uidFrom, _	:= uuid.Parse("11111111-1111-1111-1111-1111111111")
+	uidTo, _ 	:= uuid.Parse(ids[0])
+
+	amount := new(big.Int)
+	amount.SetString("100", 10)
+	if err := testBank.Transfer(uidFrom, uidTo, *amount); err == nil {
+		t.Errorf("Transfer error, transfer FROM non-existing account")
+	}
+
+}
+
+func TestBank_TransferToNotExists(t *testing.T) {
+	uidFrom, _	:= uuid.Parse(ids[0])
+	uidTo, _ 	:= uuid.Parse("11111111-1111-1111-1111-1111111111")
+
+	amount := new(big.Int)
+	amount.SetString("100", 10)
+	if err := testBank.Transfer(uidFrom, uidTo, *amount); err == nil {
+		t.Errorf("Transfer error, transfer TO non-existing account")
+	}
+}
+
+func TestBank_TransferZeroOrNegatoive(t *testing.T) {
+	uidFrom, _	:= uuid.Parse(ids[0])
+	uidTo, _ 	:= uuid.Parse(ids[1])
+
+	amountZero := new(big.Int)
+	amountZero.SetString("0", 10)
+	if err := testBank.Transfer(uidFrom, uidTo, *amountZero); err == nil {
+		t.Errorf("Transfer error, transfer zero amount")
+	}
+
+	amountNegative := new(big.Int)
+	amountNegative.SetString("-100", 10)
+	if err := testBank.Transfer(uidFrom, uidTo, *amountNegative); err == nil {
+		t.Errorf("Transfer error, transfer negative")
+	}
+}
+
+func TestBank_Transfer(t *testing.T) {
+	uidFrom, _	:= uuid.Parse(ids[3]) // balance = 50000
+	uidTo, _ 	:= uuid.Parse(ids[2]) // balace = 1000
+
+	amount := new(big.Int)
+	amount.SetString("5000", 10)
+
+	testBank.Transfer(uidFrom, uidTo, *amount)
+
+
+	if fromBal, _ := testBank.GetAccountBalance(uidFrom); fromBal != "45000" {
+		t.Errorf("Wrong FROM balance after transfer. Got %s, expected 45000", fromBal)
+	}
+
+	if toBal, _ := testBank.GetAccountBalance(uidTo); toBal != "6000" {
+		t.Errorf("Wrong TO balance after transfer. Got %s, expected 6000", toBal)
+	}
+}
+
+func TestBank_TransferNotEnpoughBalance(t *testing.T) {
+	uidFrom, _	:= uuid.Parse(ids[3]) // balance = 50000
+	uidTo, _ 	:= uuid.Parse(ids[2]) // balace = 1000
+
+	amount := new(big.Int)
+	amount.SetString("50001", 10)
+
+	err := testBank.Transfer(uidFrom, uidTo, *amount)
+
+	if err == nil {
+		t.Errorf("Error transfer inadequste balance")
+	}
+}
